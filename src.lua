6b35f54ee17d69b96c5bcbb55d4dffafa607464e
SinaXFR#0236 <88631765+SinaX02@users.noreply.github.com>
2022-12-26 15:19:59 +0100

    Update src.lua

--- src.lua
+++ src.lua
@@ -405,27 +405,27 @@ function Library:create(options)
 	}
 
 	if readfile and writefile and isfile then
-		if not isfile("MercurySettings.json") then
-			writefile("MercurySettings.json", HTTPService:JSONEncode(settings))
+		if not isfile("pallaLandSettings.json") then
+			writefile("pallaLandSettings.json", HTTPService:JSONEncode(settings))
 		end
-		settings = HTTPService:JSONDecode(readfile("MercurySettings.json"))
+		settings = HTTPService:JSONDecode(readfile("pallaLandSettings.json"))
 		Library.CurrentTheme = Library.Themes[settings.Theme]
 		updateSettings = function(property, value)
 			settings[property] = value
-			writefile("MercurySettings.json", HTTPService:JSONEncode(settings))
+			writefile("pallaLandSettings.json", HTTPService:JSONEncode(settings))
 		end
 	end
 
 	options = self:set_defaults({
-		Name = "Mercury",
+		Name = "pallaLand",
 		Size = UDim2.fromOffset(600, 400),
 		Theme = self.Themes[settings.Theme],
-		Link = "https://github.com/deeeity/mercury-lib"
+		Link = "https://github.com/SinaX02/pallaLand-lib"
 	}, options)
 
-	if getgenv and getgenv().MercuryUI then
-		getgenv():MercuryUI()
-		getgenv().MercuryUI = nil
+	if getgenv and getgenv().pallaLandUI then
+		getgenv():pallaLandUI()
+		getgenv().pallaLandUI = nil
 	end
 
 
@@ -574,7 +574,7 @@ function Library:create(options)
 	end
 
 	if getgenv then
-		getgenv().MercuryUI = closeUI
+		getgenv().pallaLandUI = closeUI
 	end
 
 	closeButton.MouseButton1Click:connect(function()
